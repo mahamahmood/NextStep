821729e448b763c4ea6361a0c47f9adf1a99258f
Maha Mahmood <>
2020-10-26 20:52:08 -0400

    styled app bg

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
       {loading && <h1>Loading ...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map(job => {
