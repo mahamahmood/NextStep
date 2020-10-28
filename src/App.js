999d810a4ec83be1a18cfd1c4e280218b74ef824
Maha Mahmood <>
2020-10-28 19:13:16 -0400

    created an ability to search for postions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
   //useFetchJobs: a custom hook. when we call the api, we'll have jobs, loading, & error states.
@@ -10,9 +11,19 @@ function App() {
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(pervParams => {
+      return { ...pervParams, [param]: value}
+    })
+  }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
       {loading && <h1>Loading ...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
